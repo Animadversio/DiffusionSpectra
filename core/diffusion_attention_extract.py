85877ed009a937a4447b1fecb86fac09f8fb6b6c
Binxu <binxu_wang@hms.harvard.edu>
2022-12-21 16:28:43 -0500

    extract and visualize attention masks during diffusion process. Add plotting function to average over heads, time etc.

--- core/diffusion_attention_extract.py
+++ core/diffusion_attention_extract.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from diffusers import StableDiffusionPipeline  #, EulerDiscreteScheduler
 
-exproot = r"/home/binxuwang/insilico_exp/Diffusion_Hessian/StableDiffusion"
+exproot = r"/home/binxuwang/insilico_exp/Diffusion_AttnMap/StableDiffusion"
 pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
     revision="fp16",
@@ -18,8 +18,10 @@ pipe.enable_attention_slicing()
 pipe.text_encoder.requires_grad_(False)
 pipe.unet.requires_grad_(False)
 pipe.vae.requires_grad_(False)
+
 #%% Extract the attention mask from the model
 from collections import defaultdict
+from diffusers.models.attention import CrossAttention
 
 def hook_forger(key, ingraph=False):
     def output_hook(module, input, output):
@@ -33,12 +35,10 @@ def hook_spatial_transformer(module, module_id):
     """Hook the spatial transformer module to extract the QK for computing attention map"""
     h1 = module.transformer_blocks[0].attn1.to_q.register_forward_hook(hook_forger(module_id+"_SelfA_Q"))
     h2 = module.transformer_blocks[0].attn1.to_k.register_forward_hook(hook_forger(module_id+"_SelfA_K"))
-
     h3 = module.transformer_blocks[0].attn2.to_q.register_forward_hook(hook_forger(module_id+"_CrosA_Q"))
     h4 = module.transformer_blocks[0].attn2.to_k.register_forward_hook(hook_forger(module_id+"_CrosA_K"))
     return [h1, h2, h3, h4]
-#%%
-from diffusers.models.attention import CrossAttention
+
 #%% Extract all attention modules
 attn_module_dict = [
     ("down00", pipe.unet.down_blocks[0].attentions[0]),
@@ -56,25 +56,42 @@ attn_module_dict = [
     ("up31", pipe.unet.up_blocks[3].attentions[1]),
 ]
 #%%
+
+#%%
+for label, module in attn_module_dict:
+    print(label, module.transformer_blocks[0].attn1.heads)
+    print(label, module.transformer_blocks[0].attn2.heads)
+# all attention modules have 8 heads.
+#%%
+# 42
+RNGseed = 82
+RNG = torch.cuda.manual_seed(RNGseed)
+activation = defaultdict(list)
+# prompt = "a cute and classy mouse lady wearing dress walking on heels"
+prompt = "a fluffy cat throwing a basketball on a playground"
+
+text_inputs = pipe.tokenizer(prompt, padding="max_length",
+    max_length=pipe.tokenizer.model_max_length,
+    return_tensors="pt",
+)
+tokens_ids_val = text_inputs.input_ids[0][text_inputs.attention_mask[0].bool()]
+tokens_val = pipe.tokenizer.convert_ids_to_tokens(tokens_ids_val)
+
 hook_handles = []
 for module_id, module in attn_module_dict:
     handles = hook_spatial_transformer(module, module_id)
     hook_handles.extend(handles)
-#%%
-activation = defaultdict(list)
-prompt = "a cute and classy mice wearing dress and heels"
 with torch.no_grad():
-    out = pipe(prompt, )
+    out = pipe(prompt, generator=RNG, )
 
 out.images[0].show()
-# %
+
 for hook in hook_handles:
     hook.remove()
-#%%
-print([*activation])
-#%%
+
 for key in activation:
     activation[key] = torch.stack(activation[key], dim=0)
+print([*activation])
 #%%
 module_id = "mid00" #"up10"#"down20"
 print(activation[module_id+"_CrosA_Q"].shape)
@@ -104,19 +121,45 @@ plt.figure(figsize=(10, 10))
 plt.imshow(out.images[0])
 plt.show()
 #%%
+import json
+from einops import rearrange
+def Attention_map_from_QK(Q: torch.Tensor, K: torch.Tensor,
+                            n_head:int = 8, ):
+    """
+    :param Q:  shape [B, N, C]
+    :param K:  shape [B, M, C]
+    :return:   shape [B, h, N, M]
 
-#%%
-text_inputs = pipe.tokenizer(prompt, padding="max_length",
-    max_length=pipe.tokenizer.model_max_length,
-    return_tensors="pt",
-)
-tokens_ids_val = text_inputs.input_ids[0][text_inputs.attention_mask[0].bool()]
-tokens_val = pipe.tokenizer.convert_ids_to_tokens(tokens_ids_val)
-#%%
+    or time batched version
 
-#%%
+    :param Q:  shape [T, B, N, C]
+    :param K:  shape [T, B, M, C]
+    :return:   shape [T, B, h, N, M]
+    """
+    if Q.ndim == 4 and K.ndim == 4:
+        batch = True
+        T_dim, B_dim = Q.shape[0], Q.shape[1]
+        Q = rearrange(Q, "T B I H -> (T B) I H")
+        K = rearrange(K, "T B J H -> (T B) J H")
+    else:
+        batch = False
+        T_dim, B_dim = 1, Q.shape[0]
+    Q_head = rearrange(Q, "B I (h H) -> (B h) I H", h=n_head)
+    K_head = rearrange(K, "B J (h H) -> (B h) J H", h=n_head)
+    scale = K.shape[-1] ** -0.5
+    dotmap = scale * torch.bmm(Q_head.float(), K_head.transpose(-1, -2).float(),)
+    scoremap = torch.softmax(dotmap, dim=-1)
+    dotmap = rearrange(dotmap, "(B h) I J -> B h I J", h=n_head)
+    scoremap = rearrange(scoremap, "(B h) I J -> B h I J", h=n_head)
+    if batch:
+        dotmap = rearrange(dotmap, "(T B) h I J -> T B h I J", T=T_dim, B=B_dim)
+        scoremap = rearrange(scoremap, "(T B) h I J -> T B h I J", T=T_dim, B=B_dim)
+    return dotmap, scoremap
+
+
 from core.utils.plot_utils import saveallforms
-def visualize_dot_maps(dotmap, tokens_val, module_id, tstep, expdir, save=True):
+def visualize_dot_maps(dotmap, tokens_val, module_id, tstep,
+                       expdir, fnstr="", save=True):
     """
 
     :param dotmap:  shape [2, S x S, 77]
@@ -142,8 +185,97 @@ def visualize_dot_maps(dotmap, tokens_val, module_id, tstep, expdir, save=True):
             ax.axis("off")
         plt.tight_layout()
         if save:
-            saveallforms(expdir, f"{module_id}_CrossAttnMap_t{tstep}_Lyr{Lyr}")
             # plt.savefig(join(expdir, f"{module_id}_CrossAttnMap_t{tstep}.png"))
+            saveallforms(expdir, f"{module_id}_CrossAttnMap_t{tstep}_Lyr{Lyr}{fnstr}")
+            plt.close()
+        else:
+            plt.show()
+    return fig
+
+
+def visualize_dot_maps_seris(dotmap, tokens_val, module_id,
+                             tsteps, expdir, fnstr="", save=True, colorbar=True):
+    """
+
+    :param dotmap:  shape [T, 2, S x S, 77]
+    :param tokens_val:  shape [T] smaller than 77, the actual tokens without paddings
+    :param module_id:  the module id to print on title
+    :param tstep:    the time step to print on title
+    :param expdir:  the directory to save the figure
+    :return:
+    """
+    space_dim = int(np.sqrt(dotmap.shape[2]))
+    for Lyr in range(dotmap.shape[1]):
+        fig, axs = plt.subplots(ncols=len(tsteps), nrows=len(tokens_val),
+                        figsize=(len(tsteps) * 2, len(tokens_val) * 2))
+        for icol, ti in enumerate(tsteps):
+            for irow in range(len(tokens_val)):
+                tok_loc = irow
+                ax = axs[irow, icol]
+                cim = ax.imshow(dotmap[ti, Lyr, :, tok_loc].reshape(space_dim, space_dim)\
+                                .detach().cpu().numpy(), cmap="rocket")
+                if colorbar: fig.colorbar(cim, ax=ax)
+                ax.axis("image")
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+        for irow, tok in enumerate(tokens_val):
+            ax = axs[irow, 0]
+            ax.set_ylabel(tok, fontsize=16)
+        for icol, ti in enumerate(tsteps):
+            ax = axs[0, icol]
+            ax.set_title(f"t={ti}", fontsize=16)
+
+        plt.suptitle(f"Cross Attention (Dot prod) Map across time\nModule {module_id} Lyr {Lyr}",
+                     fontsize=20)
+        plt.tight_layout()
+        if save:
+            saveallforms(expdir, f"{module_id}_CrossAttnMap_t_series_Lyr{Lyr}{fnstr}")
+            plt.close()
+        else:
+            plt.show()
+    return fig
+
+
+def visualize_dot_maps_heads(dotmap, tokens_val, module_id,
+                             expdir, fnstr="", save=True, colorbar=True):
+    """
+
+    :param dotmap:  shape [2, H, S x S, 77]
+    :param tokens_val:  shape [T] smaller than 77, the actual tokens without paddings
+    :param module_id:  the module id to print on title
+    :param tstep:    the time step to print on title
+    :param expdir:  the directory to save the figure
+    :return:
+    """
+    space_dim = int(np.sqrt(dotmap.shape[2]))
+    n_heads = dotmap.shape[1]
+    for Lyr in range(dotmap.shape[0]):
+        fig, axs = plt.subplots(ncols=n_heads, nrows=len(tokens_val),
+                        figsize=(n_heads * 2, len(tokens_val) * 2))
+        for icol, hi in enumerate(range(n_heads)):
+            for irow in range(len(tokens_val)):
+                tok_loc = irow
+                ax = axs[irow, icol]
+                cim = ax.imshow(dotmap[Lyr, hi, :, tok_loc].reshape(space_dim, space_dim)\
+                                .detach().cpu().numpy(), cmap="rocket")
+                if colorbar: fig.colorbar(cim, ax=ax)
+                ax.axis("image")
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+        for irow, tok in enumerate(tokens_val):
+            ax = axs[irow, 0]
+            ax.set_ylabel(tok, fontsize=16)
+        for icol, hi in enumerate(range(n_heads)):
+            ax = axs[0, icol]
+            ax.set_title(f"head {hi}", fontsize=16)
+
+        plt.suptitle(f"Cross Attention (Dot prod) Map n heads\nModule {module_id} Lyr {Lyr}",
+                     fontsize=20)
+        plt.tight_layout()
+        if save:
+            saveallforms(expdir, f"{module_id}_CrossAttnMap_nheads_Lyr{Lyr}{fnstr}")
             plt.close()
         else:
             plt.show()
@@ -154,8 +286,7 @@ def visualize_dot_maps(dotmap, tokens_val, module_id, tstep, expdir, save=True):
 exproot = r"/home/binxuwang/insilico_exp/Diffusion_AttnMap/StableDiffusion"
 expdir = join(exproot, "cute_classy_mice")
 os.makedirs(expdir, exist_ok=True)
-for module_id in tqdm(["down00", "down01", "down10", "down11", "down20", "down21",
-                  "mid00", "up10", "up11", "up20", "up21", "up30", "up31"]):
+for module_id, _ in tqdm(attn_module_dict):
     for tstep in tqdm(range(0, 51, 5)):
         Q = activation[module_id+"_CrosA_Q"]
         K = activation[module_id+"_CrosA_K"]
@@ -163,16 +294,50 @@ for module_id in tqdm(["down00", "down01", "down10", "down11", "down20", "down21
         dotmap = scale * torch.bmm(Q[tstep].float(), K[tstep].transpose(-1, -2).float(),)
         scoremap = torch.softmax(dotmap, dim=-1)
         visualize_dot_maps(dotmap, tokens_val, module_id, tstep, expdir)
-        # for Lyr in range(0, 1):
-        #     plt.figure(figsize=(10, 10))
-        #     sns.heatmap(dotmap[Lyr, :, 51].reshape(16, 16).detach().cpu().numpy())
-        #     plt.axis("image")
-        #     plt.savefig(join(exproot, f"{module_id}_CrossAttnMap_t{tstep}_L{Lyr}.png"))
-        #     plt.close()
 #%%
 attn_temp = pipe.unet.down_blocks[0].attentions[0].transformer_blocks[0].attn1
 #%%
-from einops import rearrange
+
+module_id = "up10" # "down10"; "mid00" #"up10"#"down20"
+Q = activation[module_id+"_CrosA_Q"]
+K = activation[module_id+"_CrosA_K"]
+dotmap_tsr, scoremap_tsr = Attention_map_from_QK(Q, K,)
+# dotmap_tsr.shape:
+# [Timestep, Batch, Head, N_imgpatch, M_texttoken]
+# Batch = 2
+#%%
+visualize_dot_maps(scoremap_tsr[:, 1:, :, :, :].mean(dim=(0,2)),
+             tokens_val, module_id, 0, "", save=False, fnstr="_THAvg")
+#%%
+visualize_dot_maps_seris(scoremap_tsr[:, 1:].mean(dim=2),
+             tokens_val, module_id, range(0, 51, 5), "", save=False, colorbar=False, fnstr="_headAvg")
+#%%
+visualize_dot_maps_heads(scoremap_tsr[:, 1:].mean(dim=0),
+             tokens_val, module_id, "", save=False, colorbar=False, fnstr="_tAvg")
+
+#%%
+expname = "_".join(prompt.split())
+expdir = join(exproot, "_".join(prompt.split()))
+# expdir = join(exproot, "cute_classy_mice_walk")
+os.makedirs(expdir, exist_ok=True)
+
+json.dump({"prompt":prompt, "seed":RNGseed},
+          open(join(expdir, "prompt_cfg.json"), "w"))
+out.images[0].save(join(expdir, "sample.png"))
+torch.save(activation, join(expdir, "AttnQK.pt"))
+for module_id, _ in tqdm(attn_module_dict):
+    Q = activation[module_id+"_CrosA_Q"]
+    K = activation[module_id+"_CrosA_K"]
+    dotmap_tsr, scoremap_tsr = Attention_map_from_QK(Q, K,)
+    visualize_dot_maps(scoremap_tsr[:, 1:, :, :, :].mean(dim=(0,2)),
+                       tokens_val, module_id, 50, expdir, save=True, fnstr="_THAvg")
+    visualize_dot_maps_seris(scoremap_tsr[:, 1:].mean(dim=2),
+                     tokens_val, module_id, range(0, 51, 5), expdir, save=True, colorbar=False, fnstr="_headAvg")
+    visualize_dot_maps_heads(scoremap_tsr[:, 1:].mean(dim=0),
+                     tokens_val, module_id, expdir, save=True, colorbar=False, fnstr="_tAvg")
+#%%
+
+#%%
 module_id =  "up20"
 Q = activation[module_id+"_CrosA_Q"]
 K = activation[module_id+"_CrosA_K"]
@@ -187,7 +352,7 @@ scoremap_tsr = rearrange(scoremaps, "(T B h) I K -> T B h I K", B=2, h=8)
 # Q_head2 = attn_temp.reshape_heads_to_batch_dim(Q_Tmerg)
 # torch.allclose(Q_head, Q_head2, rtol=1E-4)
 #%%
-figh = visualize_dot_maps(scoremap_tsr[:,:,:,:,:].mean(dim=(0,2)), tokens_val, module_id, tstep, expdir,save=False)
+figh = visualize_dot_maps(scoremap_tsr[:, :, :, :, :].mean(dim=(0,2)), tokens_val, module_id, tstep, expdir,save=False)
 figh.show()
 #%%
 figh = visualize_dot_maps(dotmap, tokens_val, module_id, tstep, expdir)
