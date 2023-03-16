0d2de7040a74e1721ac4ad3fb33023508566fe12
BinxuWang <binxu.wang@wustl.edu>
2023-03-15 21:46:22 -0500

    Refactor code add playground and library

---
+++ core/gmm_general_diffusion_dynamics.py
@@ -0,0 +1,127 @@
+"""Playground for Gaussian mixture model diffusion dynamics
+wit general covariance matrix.
+Reverse diffusion 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from core.gmm_general_diffusion_lib import exact_general_gmm_reverse_diff, \
+    demo_gaussian_mixture_diffusion, _random_orthogonal_matrix, \
+    gaussian_mixture_logprob_score
+#%%
+mus = np.array([[0, 0],
+               [1, 1],
+               [.5, .5], ]) # [N comp, N dim]
+Lambdas = np.array([[.8, .2],
+                   [.5, .2],
+                   [.2, .8],]) # [N comp, N dim]
+Us = np.stack([_random_orthogonal_matrix(2) for i in range(3)], axis=0)
+#%%
+xx, yy = np.meshgrid(np.linspace(-4, 4, 100), np.linspace(-4, 4, 100))
+pnts = np.stack([xx.ravel(), yy.ravel()], axis=-1)
+logprob_pnts, score_pnts = gaussian_mixture_logprob_score(pnts, mus=mus, Us=Us, Lambdas=Lambdas, weights=None)
+#%%
+slc = slice(None, None, 5)
+plt.figure(figsize=[8, 8])
+plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
+plt.colorbar()
+plt.quiver(xx[slc,slc], yy[slc,slc],
+           score_pnts[:, 0].reshape(xx.shape)[slc,slc],
+           score_pnts[:, 1].reshape(xx.shape)[slc,slc])
+plt.axis("image")
+plt.tight_layout()
+plt.show()
+#%%
+demo_gaussian_mixture_diffusion(500, mus, Us, Lambdas, )
+
+#%%
+
+
+#%%
+xT = np.random.randn(2)  # np.array([1.5, 0.5])
+t_eval = np.linspace(1, 0, 100)
+x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=t_eval)
+#%%
+plt.figure()
+plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
+plt.plot(sol.y[0, :], sol.y[1, :], 'r')
+plt.plot(xT[0], xT[1], 'rx')
+plt.plot(x0[0], x0[1], 'bo')
+plt.show()
+#%%
+sol_col = []
+for i in range(500):
+    xT = np.random.randn(2)
+    x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=t_eval)
+    sol_col.append(sol)
+
+x0_col = [sol.y[:, -1] for sol in sol_col]
+xT_col = [sol.y[:, 0] for sol in sol_col]
+x0_col = np.stack(x0_col, axis=0)
+xT_col = np.stack(xT_col, axis=0)
+#%%
+plt.figure(figsize=(8, 8))
+plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
+for i, sol in enumerate(sol_col):
+    plt.plot(sol.y[0, :], sol.y[1, :], c="k", alpha=0.1, lw=0.75,label=None if i > 0 else "trajectories")
+plt.scatter(x0_col[:, 0], x0_col[:, 1], s=40, c="b", alpha=0.3, label="final x0")
+plt.scatter(xT_col[:, 0], xT_col[:, 1], s=40, c="k", alpha=0.1, label="initial xT")
+plt.scatter(mus[:, 0], mus[:, 1], s=64, c="r", alpha=0.3, label="GMM centers")
+plt.axis("image")
+plt.legend()
+plt.tight_layout()
+plt.show()
+#
+
+#%% test unimodal Gaussian
+# ndim = 2
+# x = np.random.randn(10, ndim)  # [N batch, N dim]
+# mu = np.random.randn(ndim)
+# U = _random_orthogonal_matrix(ndim)
+# Lambda = np.exp(5 * np.random.rand(ndim))  # np.array([5, 1])
+# logdetSigma = np.sum(np.log(Lambda))
+# residual = (x - mu[None, :])  # [N batch, N dim]
+# rot_residual = residual @ U   # [N batch, N dim]
+# MHdist = np.sum(rot_residual ** 2 / Lambda[None, :], axis=-1)  # [N batch,]
+# logprob = - 0.5 * (logdetSigma + MHdist) - 0.5 * ndim * np.log(2 * np.pi)  # [N batch,]
+# score_vec = - (rot_residual / Lambda[None, :]) @ U.T  # [N batch, N dim]
+# cov = U @ np.diag(Lambda) @ U.T
+# logprob_scipy = multivariate_normal.logpdf(x, mean=mu, cov=cov)
+# assert np.allclose(logprob, logprob_scipy)
+# #%%
+# # gaussian mixture density from scipy
+
+#%% Test mulit modal GMM mixture
+# # a function of Multivariate Gaussian density
+# mus = np.array([[0, 0],
+#                [1, 1],
+#                [.5, .5],]) # [N comp, N dim]
+# Lambdas = np.array([[5, 1],
+#                    [1, 5],
+#                    [1, 1],]) # [N comp, N dim]
+# Us = np.stack([_random_orthogonal_matrix(2) for i in range(3)], axis=0)
+# # cov = U @ np.diag(Lambda) @ U.T
+# weights = np.array([0.3, 0.3, 0.4])  # [N comp,]
+# #%%
+# x = np.random.randn(10, 2)  # [N batch, N dim]
+#
+#
+# ndim = x.shape[-1]
+# alpha_t_sq = alpha(0.5)
+# sigma_t_sq = (1 - alpha_t_sq)
+# Lambdas_t = Lambdas * alpha_t_sq + sigma_t_sq  # [N comp, N dim]
+#
+#
+# logdetSigmas = np.sum(np.log(Lambdas_t), axis=-1)  # [N comp,]
+# residuals = (x[:, None, :] - mus[None, :, :])  # [N batch, N comp, N dim]
+# # `Us` has shape [N comp, N dim, N dim]
+# rot_residuals = np.einsum("BCD,CDE->BCE", residuals, Us)  # [N batch, N comp, N dim]
+# MHdists = np.sum(rot_residuals ** 2 / Lambdas_t[None, :, :], axis=-1)  # [N batch, N comp]
+# if weights is not None:
+#     logprobs = - 0.5 * (logdetSigmas[None, :] + MHdists) + np.log(weights) # - 0.5 * ndim * np.log(2 * np.pi)  # [N batch, N comp]
+# else:
+#     logprobs = - 0.5 * (logdetSigmas[None, :] + MHdists)
+# participance = softmax(logprobs, axis=-1)  # [N batch, N comp]
+# compo_score_vecs = np.einsum("BCD,CED->BCE", - (rot_residuals / Lambdas_t[None, :, :]), Us)  # [N batch, N comp, N dim]
+# score_vecs = np.einsum("BC,BCE->BE", participance, compo_score_vecs)  # [N batch, N dim]
+# prob = np.exp(logprobs).sum(axis=-1) / (2 * np.pi) ** (ndim / 2)  # [N batch,]
+
--- core/gmm_general_diffusion_lib.py
+++ core/gmm_general_diffusion_lib.py
@@ -9,6 +9,7 @@ def _random_orthogonal_matrix(n):
     Q, R = np.linalg.qr(np.random.randn(n, n))
     return Q
 
+
 def gaussian_logprob_score(x, mu, U, Lambda):
     ndim = x.shape[-1]
     logdetSigma = np.sum(np.log(Lambda))
@@ -75,7 +76,7 @@ def gaussian_mixture_score(x, mus, Us, Lambdas, weights=None):
        :param Lambdas: [N comp, N dim]
        :param weights: [N comp,] or None
        :return:
-       """
+    """
     ndim = x.shape[-1]
     logdetSigmas = np.sum(np.log(Lambdas), axis=-1)  # [N comp,]
     residuals = (x[:, None, :] - mus[None, :, :])  # [N batch, N comp, N dim]
@@ -165,6 +166,7 @@ def test_gaussian_mixture_bimodal_case_stable(ncomp=2, ndim=3, npnts=10):
     # Also note that the scipy method is not numerically stable, esp in higher dimensions
 
 
+# use gmm to define a score function, and then run the VP-ode
 def beta(t):
     return (0.02 * t + 0.0001 * (1 - t)) * 1000
 
@@ -207,7 +209,7 @@ def f_VP_gmm_vec(t, x, mus, Us, Lambdas, sigma=1E-6, weights=None):
                                   Lambdas=Lambdas_t, weights=weights).T)
 
 
-def f_VP_gmm_noise_vec(t, x, mus, sigma=1E-6, noise_std=0.01):
+def f_VP_gmm_noise_vec(t, x, mus, Us, Lambdas, sigma=1E-6, noise_std=0.01, weights=None):
     alpha_t = alpha(t)
     beta_t = beta(t)
     Lambdas_t = Lambdas * alpha_t**2 + 1 - alpha_t**2
@@ -216,19 +218,21 @@ def f_VP_gmm_noise_vec(t, x, mus, sigma=1E-6, noise_std=0.01):
           Lambdas=Lambdas_t, weights=weights).T + noise_std * np.random.randn(*x.shape))
 
 
-def exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=None):
+def exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, weights=None, t_eval=None):
     sol = solve_ivp(lambda t, x: f_VP_gmm_vec(t, x, mus=mus, Us=Us, Lambdas=Lambdas, sigma=1E-6, weights=weights),
                     (1, 0), xT, method="RK45",
                     vectorized=True, t_eval=t_eval)
     return sol.y[:, -1], sol
 
 
-def demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None):
+# Example to visualize the score function
+def demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None, weights=None):
+    import matplotlib.pyplot as plt
     ndim = 2
     if mus is None:
         mus = np.array([[0, 0],
-                        [1, 1],
-                        [.5, .5], ])  # [N comp, N dim]
+                        [-1, 1],
+                        [2, 2], ])  # [N comp, N dim]
     if Lambdas is None:
         Lambdas = np.array([[.8, .2],
                         [.5, .2],
@@ -239,11 +243,14 @@ def demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None):
     xx, yy = np.meshgrid(np.linspace(-4, 4, 100), np.linspace(-4, 4, 100))
     pnts = np.stack([xx.ravel(), yy.ravel()], axis=-1)
     logprob_pnts, score_pnts = gaussian_mixture_logprob_score(pnts,
-                               mus=mus, Us=Us, Lambdas=Lambdas, weights=None)
+                               mus=mus, Us=Us, Lambdas=Lambdas, weights=weights)
+
+    t_eval = np.linspace(1, 0, 100)
     sol_col = []
     for i in range(nreps):
         xT = np.random.randn(2)
-        x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=t_eval)
+        x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT,
+                                     weights=weights, t_eval=t_eval)
         sol_col.append(sol)
 
     x0_col = [sol.y[:, -1] for sol in sol_col]
@@ -260,128 +267,20 @@ def demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None):
     plt.scatter(x0_col[:, 0], x0_col[:, 1], s=40, c="b", alpha=0.3, label="final x0", marker="o")
     plt.scatter(xT_col[:, 0], xT_col[:, 1], s=40, c="k", alpha=0.1, label="initial xT", marker="x")
     plt.scatter(mus[:, 0], mus[:, 1], s=64, c="r", alpha=0.3, label="GMM centers")
-    plt.axis("image")
     plt.legend()
+    plt.axis("image")
     plt.tight_layout()
     plt.show()
 
-#%%
-demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None)
-#%%
-test_gaussian_logprob_score(10)
-test_gaussian_mixture_unimodal_case(ndim=10, npnts=10)
-# test_gaussian_mixture_bimodal_case(ncomp=10, ndim=100, npnts=10)
-test_gaussian_mixture_bimodal_case_stable(ncomp=10, ndim=500, npnts=10)
-#%%
-mus = np.array([[0, 0],
-               [1, 1],
-               [.5, .5], ]) # [N comp, N dim]
-Lambdas = np.array([[.8, .2],
-                   [.5, .2],
-                   [.2, .8],]) # [N comp, N dim]
-Us = np.stack([_random_orthogonal_matrix(2) for i in range(3)], axis=0)
-# cov = U @ np.diag(Lambda) @ U.T
-# weights = np.array([0.3, 0.3, 0.4])
-#%%
-xx, yy = np.meshgrid(np.linspace(-4, 4, 100), np.linspace(-4, 4, 100))
-pnts = np.stack([xx.ravel(), yy.ravel()], axis=-1)
-logprob_pnts, score_pnts = gaussian_mixture_logprob_score(pnts, mus=mus, Us=Us, Lambdas=Lambdas, weights=None)
-#%%
-import matplotlib.pyplot as plt
-plt.figure()
-plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
-plt.colorbar()
-plt.show()
-# plt.figure()
-# plt.quiver(xx, yy, score_pnts[:, 0].reshape(xx.shape), score_pnts[:, 1].reshape(xx.shape))
-# plt.colorbar()
-# plt.show()
+
+if __name__ == "__main__":
+    #%%
+    demo_gaussian_mixture_diffusion(nreps=500, mus=None, Us=None, Lambdas=None)
+    test_gaussian_logprob_score(10)
+    test_gaussian_mixture_unimodal_case(ndim=10, npnts=10)
+    test_gaussian_mixture_bimodal_case(ncomp=10, ndim=100, npnts=10)
+    test_gaussian_mixture_bimodal_case_stable(ncomp=10, ndim=500, npnts=10)
 #%%
 
 
-xT = np.random.randn(2)  # np.array([1.5, 0.5])
-t_eval = np.linspace(1, 0, 100)
-x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=t_eval)
-#%%
-plt.figure()
-plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
-plt.plot(sol.y[0, :], sol.y[1, :], 'r')
-plt.plot(xT[0], xT[1], 'rx')
-plt.plot(x0[0], x0[1], 'bo')
-plt.show()
-#%%
-sol_col = []
-for i in range(500):
-    xT = np.random.randn(2)
-    x0, sol = exact_general_gmm_reverse_diff(mus, Us, Lambdas, xT, t_eval=t_eval)
-    sol_col.append(sol)
 
-x0_col = [sol.y[:, -1] for sol in sol_col]
-xT_col = [sol.y[:, 0] for sol in sol_col]
-x0_col = np.stack(x0_col, axis=0)
-xT_col = np.stack(xT_col, axis=0)
-#%%
-plt.figure(figsize=(8, 8))
-plt.contour(xx, yy, logprob_pnts.reshape(xx.shape), 30)
-for i, sol in enumerate(sol_col):
-    plt.plot(sol.y[0, :], sol.y[1, :], c="k", alpha=0.1, lw=0.75,label=None if i > 0 else "trajectories")
-plt.scatter(x0_col[:, 0], x0_col[:, 1], s=40, c="b", alpha=0.3, label="final x0")
-plt.scatter(xT_col[:, 0], xT_col[:, 1], s=40, c="k", alpha=0.1, label="initial xT")
-plt.scatter(mus[:, 0], mus[:, 1], s=64, c="r", alpha=0.3, label="GMM centers")
-plt.axis("image")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-
-#%% test unimodal Gaussian
-ndim = 2
-x = np.random.randn(10, ndim)  # [N batch, N dim]
-mu = np.random.randn(ndim)
-U = _random_orthogonal_matrix(ndim)
-Lambda = np.exp(5 * np.random.rand(ndim))  # np.array([5, 1])
-logdetSigma = np.sum(np.log(Lambda))
-residual = (x - mu[None, :])  # [N batch, N dim]
-rot_residual = residual @ U   # [N batch, N dim]
-MHdist = np.sum(rot_residual ** 2 / Lambda[None, :], axis=-1)  # [N batch,]
-logprob = - 0.5 * (logdetSigma + MHdist) - 0.5 * ndim * np.log(2 * np.pi)  # [N batch,]
-score_vec = - (rot_residual / Lambda[None, :]) @ U.T  # [N batch, N dim]
-cov = U @ np.diag(Lambda) @ U.T
-logprob_scipy = multivariate_normal.logpdf(x, mean=mu, cov=cov)
-assert np.allclose(logprob, logprob_scipy)
-#%%
-# gaussian mixture density from scipy
-#%% Dev zone
-# a function of Multivariate Gaussian density
-mus = np.array([[0, 0],
-               [1, 1],
-               [.5, .5],]) # [N comp, N dim]
-Lambdas = np.array([[5, 1],
-                   [1, 5],
-                   [1, 1],]) # [N comp, N dim]
-Us = np.stack([_random_orthogonal_matrix(2) for i in range(3)], axis=0)
-# cov = U @ np.diag(Lambda) @ U.T
-weights = np.array([0.3, 0.3, 0.4])  # [N comp,]
-#%%
-x = np.random.randn(10, 2)  # [N batch, N dim]
-
-
-ndim = x.shape[-1]
-alpha_t_sq = alpha(0.5)
-sigma_t_sq = (1 - alpha_t_sq)
-Lambdas_t = Lambdas * alpha_t_sq + sigma_t_sq  # [N comp, N dim]
-
-
-logdetSigmas = np.sum(np.log(Lambdas_t), axis=-1)  # [N comp,]
-residuals = (x[:, None, :] - mus[None, :, :])  # [N batch, N comp, N dim]
-# `Us` has shape [N comp, N dim, N dim]
-rot_residuals = np.einsum("BCD,CDE->BCE", residuals, Us)  # [N batch, N comp, N dim]
-MHdists = np.sum(rot_residuals ** 2 / Lambdas_t[None, :, :], axis=-1)  # [N batch, N comp]
-if weights is not None:
-    logprobs = - 0.5 * (logdetSigmas[None, :] + MHdists) + np.log(weights) # - 0.5 * ndim * np.log(2 * np.pi)  # [N batch, N comp]
-else:
-    logprobs = - 0.5 * (logdetSigmas[None, :] + MHdists)
-participance = softmax(logprobs, axis=-1)  # [N batch, N comp]
-compo_score_vecs = np.einsum("BCD,CED->BCE", - (rot_residuals / Lambdas_t[None, :, :]), Us)  # [N batch, N comp, N dim]
-score_vecs = np.einsum("BC,BCE->BE", participance, compo_score_vecs)  # [N batch, N dim]
-prob = np.exp(logprobs).sum(axis=-1) / (2 * np.pi) ** (ndim / 2)  # [N batch,]
